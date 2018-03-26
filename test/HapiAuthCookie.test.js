3cc6b3ec930b3e363ef022aa4cdbb21a06a7b240
CodeMangler <hsdpal@gmail.com>
2018-03-27 02:45:01 +0530

    Replace cookie initialization in TokenAuthentiationSchemePlugin with HapiAuthCookie

--- test/HapiAuthCookie.test.js
+++ test/HapiAuthCookie.test.js
@@ -7,17 +7,26 @@ chai.use(sinonChai);
 
 describe('HapiAuthCookie', () => {
   describe('#initialize', () => {
+    const cookieOptions = {
+      ttl: 1000 * 60 * 60 * 24,
+      encoding: 'base64json',
+      isSecure: false,
+      isHttpOnly: false,
+      path: '/',
+    };
+
     it('configures cookie on the server with the specified options', () => {
       const mockServer = { state: sinon.spy() };
-      const hapiAuthCookie = new HapiAuthCookie(mockServer, { name: '__AUTH' });
+      const hapiAuthCookie = new HapiAuthCookie(mockServer, { name: '__AUTH', ...cookieOptions });
       hapiAuthCookie.initialize();
-      expect(mockServer.state).to.have.been.calledWith('__AUTH', {
-        ttl: 1000 * 60 * 60 * 24,
-        encoding: 'base64json',
-        isSecure: true,
-        isHttpOnly: true,
-        path: '/',
-      });
+      expect(mockServer.state).to.have.been.calledWith('__AUTH', cookieOptions);
+    });
+
+    it('defaults to "__TOKEN_AUTH" for cookie name if an explicit name is not provided', () => {
+      const mockServer = { state: sinon.spy() };
+      const hapiAuthCookie = new HapiAuthCookie(mockServer, cookieOptions);
+      hapiAuthCookie.initialize();
+      expect(mockServer.state).to.have.been.calledWith('__TOKEN_AUTH', cookieOptions);
     });
   });
 });
