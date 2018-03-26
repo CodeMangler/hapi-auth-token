ec7887fb1f723096438f8734bf7284ec12d978cd
CodeMangler <hsdpal@gmail.com>
2018-03-26 22:20:48 +0530

    Extract auth token from a configurable cookie

--- test/HapiRequestToken.test.js
+++ test/HapiRequestToken.test.js
@@ -1,36 +1,76 @@
+/* eslint-disable no-unused-expressions */
+
 import { expect } from 'chai';
 import HapiRequestToken from '../lib/HapiRequestToken';
 
 describe('HapiRequestToken', () => {
   describe('#value', () => {
+    describe('from cookie', () => {
+      it('is sessionToken in the specified cookie when available', () => {
+        const requestStub = { state: { __AUTH: { sessionToken: 'a-token' } } };
+        const token = new HapiRequestToken(requestStub, {
+          cookie: { name: '__AUTH' },
+          header: false,
+          query: false,
+        });
+        expect(token.value).to.eq('a-token');
+      });
+
+      it('defaults to the "__TOKEN_AUTH" cookie', () => {
+        const requestStub = { state: { __TOKEN_AUTH: { sessionToken: 'a-token' } } };
+        const token = new HapiRequestToken(requestStub, { header: false, query: false });
+        expect(token.value).to.eq('a-token');
+      });
+
+      it('is null when the specified cookie is not available', () => {
+        const requestStub = { state: {} };
+        const token = new HapiRequestToken(requestStub, { header: false, query: false });
+        expect(token.value).to.be.null;
+      });
+
+      it('is null when cookie tokens are not enabled', () => {
+        const requestStub = { state: {} };
+        const token = new HapiRequestToken(requestStub, {
+          cookie: false,
+          header: false,
+          query: false,
+        });
+        expect(token.value).to.be.null;
+      });
+    });
+
     describe('from header', () => {
       it('is token value from the authorization header if present', () => {
         const requestStub = { headers: { authorization: 'Token a-token' } };
-        const token = new HapiRequestToken(requestStub);
+        const token = new HapiRequestToken(requestStub, { query: false, cookie: false });
         expect(token.value).to.eq('a-token');
       });
 
       it('is null when an authorization header is not present', () => {
         const requestStub = { headers: {} };
-        const token = new HapiRequestToken(requestStub, { query: false });
+        const token = new HapiRequestToken(requestStub, { query: false, cookie: false });
         expect(token.value).to.be.null;
       });
 
       it('is null when authorization header does not have a token', () => {
         const requestStub = { headers: { authorization: 'Basic a-token' } };
-        const token = new HapiRequestToken(requestStub, { query: false });
+        const token = new HapiRequestToken(requestStub, { query: false, cookie: false });
         expect(token.value).to.be.null;
       });
 
       it('is null when authorization header does not match the expected format', () => {
         const requestStub = { headers: { authorization: 'a-token' } };
-        const token = new HapiRequestToken(requestStub, { query: false });
+        const token = new HapiRequestToken(requestStub, { query: false, cookie: false });
         expect(token.value).to.be.null;
       });
 
       it('is null when token parsing from header has not been enabled', () => {
         const requestStub = { headers: { authorization: 'Token a-token' } };
-        const token = new HapiRequestToken(requestStub, { header: false, query: false });
+        const token = new HapiRequestToken(requestStub, {
+          header: false,
+          query: false,
+          cookie: false,
+        });
         expect(token.value).to.be.null;
       });
     });
@@ -41,13 +81,14 @@ describe('HapiRequestToken', () => {
         const token = new HapiRequestToken(requestStub, {
           query: { name: 'tokenParameter' },
           header: false,
+          cookie: false,
         });
         expect(token.value).to.eq('a-token');
       });
 
-      it('defaults to value of the token query parameter', () => {
+      it('defaults to value of the "token" query parameter', () => {
         const requestStub = { query: { token: 'a-token' } };
-        const token = new HapiRequestToken(requestStub, { header: false });
+        const token = new HapiRequestToken(requestStub, { header: false, cookie: false });
         expect(token.value).to.eq('a-token');
       });
 
@@ -56,13 +97,18 @@ describe('HapiRequestToken', () => {
         const token = new HapiRequestToken(requestStub, {
           query: { name: 'tokenParameter' },
           header: false,
+          cookie: false,
         });
         expect(token.value).to.be.null;
       });
 
       it('is null when query parameter tokens are not enabled', () => {
         const requestStub = { query: { token: 'a-token' } };
-        const token = new HapiRequestToken(requestStub, { query: false, header: false });
+        const token = new HapiRequestToken(requestStub, {
+          query: false,
+          header: false,
+          cookie: false,
+        });
         expect(token.value).to.be.null;
       });
     });
